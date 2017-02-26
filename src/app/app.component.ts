f51b3cc77fe311ecc94e041eea2ed56e79fcf2fc
David Gutierrez <dcgutie1@asu.edu>
2017-02-26 01:37:46 -0700

    The beginnings...

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `<h1>Git ur insults here!</h1>
+    <h2>Click to get insulted lel.</h2>
+    <button type="button" onClick="alert('git rekt sun')">Ya kNoW u Want tO.</button>`,
+})
+export class AppComponent  { ; }
